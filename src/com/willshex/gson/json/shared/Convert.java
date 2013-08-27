8fda739c9cb58be29c624429550e5cb888d10972
William Shakour <william.shakour@gmail.com>
2013-08-27 10:29:45 +0000

    added generic conversion methods between strings and json

---
+++ src/com/willshex/gson/json/shared/Convert.java
@@ -0,0 +1,40 @@
+//
+//  Convert.java
+//  spchopr.gson
+//
+//  Created by billy1380 on 27 Aug 2013.
+//  Copyright © 2013 Spacehopper Studios Ltd. All rights reserved.
+//
+
+package com.willshex.gson.json.shared;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+/**
+ * @author billy1380
+ * 
+ */
+public class Convert {
+	public static JsonObject toJsonObject(String json) {
+		JsonObject object = null;
+
+		if (json != null) {
+			JsonElement element = (new JsonParser()).parse(json);
+
+			if (element != null) {
+				object = element.getAsJsonObject();
+			}
+		}
+		return object;
+	}
+
+	public static String fromJsonObject(JsonObject object) {
+		String json = null;
+		if (object != null) {
+			json = object.toString();
+		}
+		return json;
+	}
+}

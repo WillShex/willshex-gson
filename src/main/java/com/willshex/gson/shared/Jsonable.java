b7d0fd92da2049c5bc6f26117e454d2b0bc1edfe
billy1380 <william.shakour@gmail.com>
2016-06-19 12:32:46 +0100

    do not cast if string is not json object

--- src/main/java/com/willshex/gson/shared/Jsonable.java
+++ src/main/java/com/willshex/gson/shared/Jsonable.java
@@ -7,26 +7,33 @@
 //
 package com.willshex.gson.shared;
 
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.willshex.utility.JsonUtils;
 
 public abstract class Jsonable {
-	public JsonObject toJson() {
+
+	public JsonObject toJson () {
 		return new JsonObject();
 	}
 
-	public void fromJson(JsonObject jsonObject) {
+	public void fromJson (JsonObject jsonObject) {
 		return;
 	}
 
-	public void fromJson(String json) {
-		fromJson((JsonObject) (new JsonParser()).parse(json));
-		return;
+	public void fromJson (String json) {
+		JsonElement element = new JsonParser().parse(json);
+
+		if (element.isJsonObject()) {
+			fromJson(element.getAsJsonObject());
+		} else {
+			// was expecting json object or null
+		}
 	}
-	
+
 	@Override
-	public String toString() {
+	public String toString () {
 		return JsonUtils.cleanJson(toJson().toString());
 	}
 }
